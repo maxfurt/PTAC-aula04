9758a5c63edfb141347e8570b506ba97332bbea8
maxfurt <marcoruckl75@Hotmail.com>
2023-08-22 16:45:46 -0400

    Agora ta safe

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
+import {ToastContainerl, toast} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Login() {
   const [user, setUser] = useState({
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,10 +1,16 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 
 export default async function Dashboard() {
-   
+
+   let usuarios = getUsers()
+
     return (
         <div>
             <h1>Dashboard</h1>
+
+            {usuarios.map((usuario) =>(
+                <h1>{usuario.nome}</h1>
+            ))}
             
         </div>
     );
