8aec94f76e70f58e906c0b678a51384bf82225c1
maxfurt <marcoruckl75@Hotmail.com>
2023-09-19 00:08:14 -0400

    Atualização estilização inicial no register

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -4,15 +4,17 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import {ToastContainer, toast} from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
+import styles from "./Register.module.css";
 
 export default function Register() {
   const [user, setUser] = useState({
+    name: '',
     email: '',
     password: '',
   });
   const { refresh } = useRouter();
 
-  const handlerLogin = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     try {
         toast.error("Usuario Registrado Com Sucesso")
@@ -23,27 +25,30 @@ export default function Register() {
   }
 
   return (
-    <div>
-      <h1>Registrar</h1>
-      <form onSubmit={handlerLogin}>
-      <input
+    <div className={styles.container}>
+      <h1 className={styles.title}>Registrar</h1>
+      <form onSubmit={handleRegister}>
+        <input
+          className={styles.input}
           placeholder='Nome'
           type="text"
-          onChange={(e) => { setUser({ ...user, text: e.target.value }) }}>
-        </input>
+          onChange={(e) => { setUser({ ...user, name: e.target.value }) }}
+        />
         <input
+          className={styles.input}
           placeholder='E-mail'
           type="email"
-          onChange={(e) => { setUser({ ...user, email: e.target.value }) }}>
-        </input>
+          onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
+        />
         <input
+          className={styles.input}
           placeholder='Senha'
           type='password'
-          onChange={(e) => { setUser({ ...user, password: e.target.value }) }}>
-        </input>
-        <button>Entrar</button>
+          onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
+        />
+        <button className={styles.button}>Registrar</button>
       </form>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
-  )
+  );
 }
