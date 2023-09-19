70591fb7ad1fff156b6d6ee20f12a6faf5be8772
maxfurt <marcoruckl75@Hotmail.com>
2023-09-18 23:54:57 -0400

    Estilização inicial da dashboard

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,20 +1,41 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
-import { Suspense } from "react"
-import  ListUsers  from "@/app/components/ListUsers"
-
+import { Suspense } from "react";
+import ListUsers from "@/app/components/ListUsers";
+import styles from "./Dashboard.module.css";
 
 export default async function Dashboard() {
+  let usuarios = getUsers();
 
-   let usuarios = getUsers()
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <a href="/" className={styles.link}>
+                Home
+              </a>
+            </li>
+            <li className={styles.li}>
+              <a href="/pages/register" className={styles.link}>
+                Register
+              </a>
+            </li>
+            <li className={styles.li}>
+              <a href="/pages/alter" className={styles.link}>
+                Alterar
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-    return (
-        <div>
-            <h1>Dashboard</h1>
-
-            <Suspense fallback={<p>carregando...</p>}>
-            <ListUsers users={usuarios}/>
-
-            </Suspense>
-        </div>
-    );
-};
+      <section className={styles.userList}>
+        <h1 className={styles.title}>Dashboard</h1>
+        <Suspense fallback={<p>Carregando...</p>}>
+          <ListUsers users={usuarios} />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
