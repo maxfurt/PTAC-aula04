3565f5c87ce700d9175ea9756052432f464e565d
LuccaRex <marcoruckl75@Hotmail.com>
2023-10-17 17:34:44 -0400

    update

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,4 +1,18 @@
 'use server'
+
+const url = "https://aula-17-10-xi.vercel.app"
+const getUserAuthenticated = async (user) =>{
+    const responseOfApi = await fetch(url + "/user/authenticated",
+    {
+        method:"POST",
+        headers:{"Content-Type":"Application/json"},
+        body: JSON.stringify(user)
+    }
+    )
+    const userAuth = await responseOfApi.json();
+    return userAuth;
+}
+
 const usuarios = [
 {
         nome:"nomemuitomaneiro",
@@ -20,7 +34,7 @@ const usuarios = [
 }]
 
 
-const getUserAuthenticated = (user) => {
+/*const getUserAuthenticated = (user) => {
 
     let userAuth = {}
 
@@ -34,7 +48,7 @@ const getUserAuthenticated = (user) => {
 
     return userAuth
 }
-
+*/
 
 const getUsers = () =>{
 
